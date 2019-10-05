197405ab863aff9ee3b7b6ba4d128ce610a0f6fb
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-10-06 01:21:21 +0900

    各ModuleのInstall

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
+const pg = require("pg");
 
-app.get("/", (req, res) => {
-  res.send("こんにちは！");
+const pool = pg.Pool({
+  database: "githubstar",
+  user: "taku",
+  password: "",
+  host: "localhost",
+  port: 5432
 });
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("こんにちは！");
 });
