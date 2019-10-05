fe12cb882fcd5ab637971dc2a83d410faddfe27d
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-10-06 02:24:50 +0900

    db.jsにPostgreのアカウントを追加、Index.jsにSELECT文の追加

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const app = express();
-const pg = require("pg");
-
-const pool = pg.Pool({
-  database: "githubstar",
-  user: "taku",
-  password: "",
-  host: "localhost",
-  port: 5432
-});
+const db = require("db");
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
 
 app.get("/", (req, res) => {
-  res.send("こんにちは！");
+  db.any("SELECT * from api_github_star")
+    .then(data => {
+      console.log(data);
+      res.send("こんにちは！");
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
