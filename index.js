552d850d4c569f0e31dd598942371ece7b4ad2a6
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-10-07 00:07:42 +0900

    Routerを使用して、フォルダを分けた

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const db = require("./db");
 const router = express.Router();
+const router = require("./router/v3");
+const db = require("./db");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use("/api/v3", router);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
 
-app.get("/test", (req, res) => {
-  db.any("SELECT * from api_github_star")
-    .then(data => {
-      console.log("test: ", data);
-      res.send("こんにちは！");
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
+// app.get("/api/v3/", (req, res) => {
+//   res.json({
+//     message: "こんにちは！"
+//   });
+// });
 
-app.get("/test", (req, res) => {
-  db.any("SELECT * from api_github_star")
-    .then(data => {
-      console.log("test: ", data);
-      res.send("こんにちは！");
-    })
-    .catch(error => {
-      console.log(error);
-    });
-});
+// app.get("/test", (req, res) => {
+//   db.any("SELECT * from api_github_star")
+//     .then(data => {
+//       console.log("test: ", data);
+//       res.send("こんにちは！");
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// });
