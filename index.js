c10de8f16e9229080fbe5b98517cd8ed27104057
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-10-06 23:58:55 +0900

    ModuleのInstall

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const app = express();
-const db = require("db");
+const bodyParser = require("body-parser");
+const db = require("./db");
+const router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
 
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   db.any("SELECT * from api_github_star")
     .then(data => {
-      console.log(data);
+      console.log("test: ", data);
+      res.send("こんにちは！");
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.get("/test", (req, res) => {
+  db.any("SELECT * from api_github_star")
+    .then(data => {
+      console.log("test: ", data);
       res.send("こんにちは！");
     })
     .catch(error => {
