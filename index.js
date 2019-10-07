b9afd5c72bc8a8557cfb87cffeafb75e609dcf3f
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-10-07 23:24:29 +0900

    コメントの削除

--- index.js
+++ index.js
@@ -8,25 +8,8 @@ const db = require("./db");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/api/v3", router);
+app.use("/api/v1", router);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
-
-// app.get("/api/v3/", (req, res) => {
-//   res.json({
-//     message: "こんにちは！"
-//   });
-// });
-
-// app.get("/test", (req, res) => {
-//   db.any("SELECT * from api_github_star")
-//     .then(data => {
-//       console.log("test: ", data);
-//       res.send("こんにちは！");
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// });
