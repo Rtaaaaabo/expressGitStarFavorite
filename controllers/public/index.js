69e9f24d7de1a59c40b5e1345892537920c28c67
Taku Nakagawa <tk@www-biz.co>
2019-10-08 18:18:36 +0900

    favarite Collectionを利用するためにpublic/Controllerを作成

--- controllers/public/index.js
+++ controllers/public/index.js
@@ -1,7 +1,15 @@
 "use strict";
+const Promise = require('bluebird');
+const FavariteCollection = require('../../collections/favariteCollection');
 
 function getIndex(req, res) {
-  res.send("こんにちは！世界！");
+  FavariteCollection.getList()
+  .then((favarite) => {
+    res.json(favarite);
+  })
+  .catch(error => {
+    res.status(500).json({msg: error.message});
+  })
 }
 
 module.exports = {
