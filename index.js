5211b1af969350e853972975e47667b92a4819e6
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-10-09 00:42:01 +0900

    API接続のためにRoot IndexにModule作成のものを追加

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
 // Config
 const config = require("./config")(process.env.ENV);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const public_index = require("./router/public/index");
 app.use("/", public_index);
-
 
 app.listen(config.port, function() {
   console.log("Example app listening on port " + config.port);
