a9c0183b00449be16de35d5d8b30e4ed7eb74ae2
Taku Nakagawa <tk@www-biz.co>
2019-10-08 17:08:38 +0900

    Bookshelf.jsを作成

--- knexfile.js
+++ knexfile.js
@@ -1,29 +1,37 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: 'githubstar',
+      user: 'nodeuser',
+      password: 'nodeuser'
+      // filename: './dev.sqlite3'
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'favorite'
     }
   },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
 
   production: {
     client: 'postgresql',
